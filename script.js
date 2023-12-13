9caf0e0478b33e2be241bf478636c7a24e3a4d7d
Alaabm <alaabasim@gmail.com>
2023-12-12 23:30:41 -0500

    color array set

---
+++ script.js
@@ -0,0 +1,6 @@
+const tiles = document.querySelector(".tiles");
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
+const colorsArray = [...colors, ...colors];
+const tileCount = colorsArray.length;
+
+console.log(colorsArray);
