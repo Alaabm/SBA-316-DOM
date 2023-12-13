97b83260629d7e8f5d9d6dc01ebf7fe58b69c350
Alaabm <alaabasim@gmail.com>
2023-12-13 01:28:26 -0500

    4x4 box completed

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ const tiles = document.querySelector(".tiles");
 const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 const colorsArray = [...colors, ...colors];
 const tileCount = colorsArray.length;
-
 console.log(colorsArray);
+
+
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfTheMove = false;
+
+for (let i = 0; i < tileCount; i++) {
+    const tile = document.createElement("div");
+    // const randomTileColor = Math.floor(Math.random() * colorsArray.length);
+    tile.classList.add("tile");
+    tile.style.backgroundColor = colorsArray[i];
+    tiles.appendChild(tile);
+
+console.log(tiles);
+}
