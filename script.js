5644082c84c64587b365bc686690f02f69bc7c46
Alaabm <alaabasim@gmail.com>
2023-12-13 16:30:02 -0500

    Added header and h1

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+// Declare variables(const)
 const tiles = document.querySelector(".tiles");
 const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 const colorsArray = [...colors, ...colors];
 const tileCount = colorsArray.length;
-console.log(colorsArray);
-
+// console.log(colors)
+// console.log(colorsArray);
 
 let revealedCount = 0;
 let activeTile = null;
 let awaitingEndOfTheMove = false;
+
+//header complete
+const header = document.querySelector("header");
+header.textContent = "Game Central";
+    
+
+//h1 complete
+const h1 = document.createElement("h1"); 
+h1.textContent = "Macthing Card Game";
+header.appendChild(h1);
+console.log(h1);
+
 
 for (let i = 0; i < tileCount; i++) {
     const tile = document.createElement("div");
@@ -15,6 +28,5 @@ for (let i = 0; i < tileCount; i++) {
     tile.classList.add("tile");
     tile.style.backgroundColor = colorsArray[i];
     tiles.appendChild(tile);
-
 console.log(tiles);
 }
